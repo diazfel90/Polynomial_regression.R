260c51f0724d372b6ed05add671e27c0328c5959
diazfel90 <39527545+diazfel90@users.noreply.github.com>
2019-01-30 20:17:21 -0600

    Update Polynomial_Regression.R

--- Polynomial_Regression.R
+++ Polynomial_Regression.R
@@ -49,3 +49,12 @@ ggplot() +
   ggtitle('Truth or Bluff (Polynomial Regression)')+
   xlab('Level')+
   ylab('Salary')
+
+# Predicting a new result with Linear Regression
+y_pred = predict(lin_reg, data.frame(Level = 6.5))
+
+# Predicting a new result with Polynomial regression
+y_pred = predict(poly_reg, data.frame(Level = 6.5,
+                                      Level2 = 6.5^2,
+                                      Level3 = 6.5^3,
+                                      Level4 = 6.5^4))
