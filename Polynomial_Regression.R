2e7502bbf7a0cf0c78f21a2c69129f38838da449
diazfel90 <39527545+diazfel90@users.noreply.github.com>
2019-01-30 20:09:05 -0600

    Update and rename README.md to Polynomial_Regression.R

---
+++ Polynomial_Regression.R
@@ -0,0 +1,51 @@
+# Polynomial Regression model
+
+# Importing the dataset
+dataset = read.csv('Position_Salaries.csv')
+dataset = dataset[2:3]
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+# library(caTools)
+# set.seed(123)
+# split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
+# training_set = subset(dataset, split == TRUE)
+# test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+# training_set = scale(training_set)
+# test_set = scale(test_set)
+
+# Fitting Linear Regression
+lin_reg = lm(formula = Salary ~ ., 
+             data = dataset)
+
+
+# Fitting Polynomial Regression to the dataset (add column for polynomial)
+dataset$Level2 = dataset$Level^2
+dataset$Level3 = dataset$Level^3
+dataset$Level4 = dataset$Level^4
+poly_reg = lm(formula = Salary ~ .,
+             data = dataset)
+
+# Visualising the linear regression results
+# install.packages('ggplot2')
+library(ggplot2)
+ggplot() + 
+  geom_point(aes(x = dataset$Level , y = dataset$Salary),
+             colour = 'red') + 
+  geom_line(aes(x = dataset$Level , y = predict(lin_reg, newdata = dataset)),
+            colour = 'blue') +
+  ggtitle('Truth or Bluff (Linear Regression)')+
+  xlab('Level')+
+  ylab('Salary')
+
+# Visualising the polynomial regression results
+ggplot() + 
+  geom_point(aes(x = dataset$Level , y = dataset$Salary),
+             colour = 'red') + 
+  geom_line(aes(x = dataset$Level , y = predict(poly_reg, newdata = dataset)),
+            colour = 'blue') +
+  ggtitle('Truth or Bluff (Polynomial Regression)')+
+  xlab('Level')+
+  ylab('Salary')
--- README.md
+++
@@ -1,1 +0,0 @@
-# Polynomial_regression.R
